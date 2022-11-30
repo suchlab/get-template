3de31c0c4574123f8053f0fa325169f3ffacac9c
Iñigo Taibo <itaibo@hey.com>
2022-11-30 16:38:11 +0100

    get-template

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # get-template
 Download any template for your project
+
+### How to use
+````sh
+npx get-template <template> [<download-directory>]
+````
+
+This will download a template in the directory you want. By default, the download directory will be the current one (i.e. `.`).
+
+Template names are GitHub's username and repository name. So, if you want to download the template from https://github.com/github/codespaces-react, your command would be:
+
+````sh
+npx get-template github/codespaces-react
+````
+
+To download it in an specific directory, your command would be:
+````sh
+npx get-template github/codespaces-react my-folder-name
+````
+
+The directory **must** be empty. If not, it won't download the template
+
+### Aliases
+To make templates easier to download, you can use aliases. These aliases simplify how people download templates.
+
+For example, the alias `package` downloads a template to [create TypeScript packages](https://github.com/itaibo/package-template):
+
+````sh
+npx get-template package
+````
+
+You can propose new alias in the [aliases repository](https://github.com/get-template/aliases)
---
+++ bin/get-template.js
@@ -0,0 +1,99 @@
+#! /usr/bin/env node
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const axios = require('axios');
+
+const ALIAS_URL = 'https://raw.githubusercontent.com/get-template/alias/main/aliases.txt';
+
+async function getTemplate() {
+	const args = process.argv.slice(2);
+
+	const template = args[0];
+	const directory = args[1] || '.';
+
+	let gitDestination;
+	
+	// Util functions
+	function error(message) {
+		console.error(`Error: ${message}`);
+		return process.exit(1);
+	}
+	
+	// Check template exists
+	if (!template) {
+		return error('A template must be specified');
+	}
+	
+	// Check directory is empty
+	try {
+		const { stdout } = await exec(`ls ${directory}`);
+		if (stdout) {
+			return error('Directory must be empty!');
+		}
+	} catch (e) {
+		if (e?.stderr && !e.stderr.includes('No such file or directory')) {
+			return error('Could not list directory');
+		}
+	}
+
+	// Check if template is an alias
+	if (!template.includes('/')) {
+		console.log('Checking template from aliases...');
+
+		let list;
+
+		try {
+			const request = await axios.get(ALIAS_URL);
+			list = request.data;
+		} catch (e) {
+			return error('Could not get aliases list');
+		}
+
+		const lines = list.split("\r\n");
+
+		for (let line of lines) {
+			line = line.trim();
+
+			if (!line) continue;
+
+			const [alias, destination] = line.split('=');
+
+			if (alias === template) {
+				gitDestination = destination;
+				console.log('Alias found!');
+				break;
+			}
+		}
+	} else {
+		gitDestination = `git@github.com:${template}.git`
+	}
+
+	// Download template
+	try {
+		console.log(`Downloading template from ${gitDestination}...`);
+		await exec(`git clone ${gitDestination} ${directory}`);
+	} catch (e) {
+		return error('Could not clone repository');
+	}
+	
+	// Remove .git
+	try {
+		console.log('Removing .git...');
+		await exec(`cd ${directory}; rm -rf .git`);
+	} catch (e) {
+		return error('Could not remove .git');
+	}
+
+	// Initialize .git
+	try {
+		console.log('Initializing new git...');
+		await exec(`cd ${directory}; git init`);
+	} catch (e) {
+		return error('Could not initialize git');
+	}
+
+	// Finish
+	console.log('Template successfully downloaded!', `You can find it ${(directory === '.' ? 'in the current directory' : `in ${directory}`)}`);
+}
+
+module.exports = getTemplate();
